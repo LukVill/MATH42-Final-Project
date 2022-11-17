a9413850a647e275eb2d748cde0a0fcde2970376
LukVill <luke.lam.villanueva@gmail.com>
2022-11-17 02:52:14 -0800

    Add project files.

---
+++ MATH42 Final Project/MATH42_Final_Project.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+url = "C:/Users/lavil/source/repos/MATH42 Final Project/NOAA_one.csv"
+outUrl = "C:/Users/lavil/source/repos/MATH42 Final Project/dailyRain.csv"
+dailyRainUrl = "C:/Users/lavil/source/repos/MATH42 Final Project/dailyRain.csv"
+
+data = pd.read_csv(url)
+
+#df holding date and hourly precipitation
+dtNrain = data[["DATE","DailyPrecipitation"]]
+
+dailyRain = dtNrain.dropna()
+
+print(dailyRain)
+
+print(dailyRain.iloc[0,0])
+"""
+##get rid of s in daily precipitation
+for x in range(len(dailyRain.columns)):
+    if dailyRain.iloc[x,1]
+"""
+"""
+## PLOTTING
+rain = pd.read_csv(dailyRainUrl)
+domain = np.linspace(1,len(rain),1)
+sns.lineplot(data = rain, x = "DATE", y = "DailyPrecipitation")
+
+plt.show()
+"""
